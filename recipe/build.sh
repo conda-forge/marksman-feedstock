a001d8de4b3b1f8ef0ebdb3dc0ba500b14602a9a
automatic conda-forge administrator <condaforge@gmail.com>
2025-04-09 07:31:37 +0200

    ENH: update package version (#5)
    * dummy commit for rerendering
    
    * update to new version
    
    * pin dotnet
    
    * fix build
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <121827174+conda-forge-webservices[bot]@users.noreply.github.com>
    Co-authored-by: Daniel Nachun <daniel.nachun@gmail.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -8,6 +8,9 @@ ln -sf ${DOTNET_ROOT}/dotnet ${PREFIX}/bin
 
 # Build package with dotnet publish
 rm global.json
+framework_version="$(dotnet --version | sed -e 's/\..*//g').0"
+sed -i "s?<TargetFramework>.*</TargetFramework>?<TargetFramework>net${framework_version}</TargetFramework>?" \
+    Marksman/Marksman.fsproj
 dotnet publish --no-self-contained Marksman/Marksman.fsproj --output ${PREFIX}/libexec/${PKG_NAME}
 rm -rf ${PREFIX}/libexec/${PKG_NAME}/${PKG_NAME}
 
